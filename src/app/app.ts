1d07b38ca9a349b9f98991b695f50177a9b3c2a0
Commoner25 <dreadedGuyNotDead>
2026-06-03 23:49:56 +0530

    File structure first draft

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Library } from './app-shell/library/library';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Library],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
