4ce7b7d71240dfdc48d4abc43c287a0dfd4286be
Commoner25 <dreadedGuyNotDead>
2026-06-23 23:22:19 +0530

    explicit rootDir declaration and Routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Mixes } from './app-shell/mixes/mixes';
+import { NowPlaying } from './app-shell/now-playing/now-playing';
+import { Library } from './app-shell/library/library';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "mixes",
+        component: Mixes
+    },
+    {
+        path: "now-playing",
+        component: NowPlaying
+    },
+    {
+        path: "library",
+        component: Library
+    }
+];
